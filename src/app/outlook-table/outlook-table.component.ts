f878f7e99189fbc6acc05ca2010415b6fddad329
trincot <51237479+trincot@users.noreply.github.com>
2024-05-07 19:42:58 +0200

    table alternative: transposed

--- src/app/outlook-table/outlook-table.component.ts
+++ src/app/outlook-table/outlook-table.component.ts
@@ -11,7 +11,11 @@ import { ApplesOutlookService } from "../apples-outlook.service";
 export class OutlookTableComponent {
   outlook: ApplesYear[] = [];
   fields: String[] = [];
-  table: Number[][] = [];
+  table: (Number | String)[][] = [];
+
+  years: String[] = [];
+  labels: String[] = [];
+  values: Number[][] = [];
 
   constructor(private applesOutlookService: ApplesOutlookService) {
 
@@ -19,11 +23,32 @@ export class OutlookTableComponent {
 
   ngOnInit(): void {
     this.getOutlook();
+    this.getOutlookTransposed();
   }
 
   getOutlook() {
     this.outlook = this.applesOutlookService.getOutlook();
-    this.fields = Object.keys(this.outlook[0]);
+    this.fields = Object.keys(this.outlook[0]).map(this.camelToWords);
     this.table = this.outlook.map(row => Object.values(row));
+  }
+  
+  getOutlookTransposed() {
+    const outlook = this.applesOutlookService.getOutlook();
+    this.labels = Object.keys(outlook[0]).slice(1).map(this.camelToWords);
+    this.years = outlook.map(obj => obj.year);
+    this.values = this.transpose(
+      outlook.map(obj => 
+        // Skip year, format numbers with 1 decimal
+        Object.values(obj).slice(1).map(val => val.toFixed(1))
+      )
+    );
+  }
+  
+  private transpose(matrix: number[][]): number[][] {
+    return matrix[0]?.map((_, i) => matrix.map(row => row[i])) ?? [];
+  }
+
+  private camelToWords(camel: string) {
+    return camel.replace(/.[a-z]*/g, word => word[0].toUpperCase() + word.slice(1) + " ").trim();
   }
 }
