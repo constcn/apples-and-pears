40b5648a605d8402db907c57b6b85bc0c30930ee
trincot <51237479+trincot@users.noreply.github.com>
2024-08-27 16:39:59 +0200

    Updated URL to local one (express)

--- src/app/apples-outlook.service.ts
+++ src/app/apples-outlook.service.ts
@@ -9,13 +9,17 @@ export class ApplesOutlookService {
   // private url = "http://localhost:3000/outlook";
   // private url = "http://209.38.232.224:8080/csv/upload";
   // private url = "https://trincot.github.io/outlook.json";
-  private url = "https://trincot.000webhostapp.com/outlook.json";
+  //private url = "https://trincot.000webhostapp.com/outlook.json";
+  private url = "http://localhost:3000/outlook.json";
 
   constructor() { }
 
   async getOutlook(): Promise<ApplesYear[]> {
     const response = await fetch(this.url, {cache: "no-cache"});
-    return (await response.json()) ?? [];
+    const data = await response.json();
+    console.log("Getting this data from server:");
+    console.log(data);
+    return data ?? [];
   }
 
 }
